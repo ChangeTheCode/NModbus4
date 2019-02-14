ce577f163931b61c098d086626ceb599f4d7fe51
Muhammad Ummar Iqbal <m.iqbal@ssidecisions.com>
2019-02-14 17:18:09 +0500

    package information updated

--- NModbus4/Properties/AssemblyInfo.cs
+++ NModbus4/Properties/AssemblyInfo.cs
@@ -7,17 +7,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NModbus4")]
 [assembly: AssemblyProduct("NSModbus4")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2006 Scott Alexander, 2015 Dmitry Turin")]
-[assembly: AssemblyDescription("NModbus4 is a C# implementation of the Modbus protocol. Provides connectivity to Modbus slave compatible devices and applications. Supports ASCII, RTU, TCP, and UDP protocols. NModbus4 it's a fork of NModbus(https://github.com/NModbus4/NModbus4)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyDescription("NModbus4 is a C# (.NetStandard 2.0) implementation of the Modbus protocol. Provides connectivity to Modbus slave compatible devices and applications. Supports ASCII, RTU, TCP, and UDP protocols. NModbus4 it's a fork of NModbus(https://github.com/NModbus4/NModbus4)")]
 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
-[assembly: AssemblyInformationalVersion("4.0.1")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
+[assembly: AssemblyInformationalVersion("4.0.2")]
 
 #if !SIGNED
 [assembly: InternalsVisibleTo("NModbus4.UnitTests")]
